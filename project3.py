b80e4fc0c0d3a0e457e7ad9c45d17775f77126bb
ThenuVijay <warunvijay@gmail.com>
2021-01-24 17:36:27 +0530

    Updated code

--- project3.py
+++ project3.py
@@ -1,36 +1,15 @@
-import nltk
-import bs4 as bs
 import urllib.request
-import re
-from nltk.util import ngrams
-from collections import Counter
+from read import *
 
 file = urllib.request.urlopen('https://en.wikipedia.org/wiki/Life')
 file1 = file.read()
 file.close()
 
-content = bs.BeautifulSoup(file1, 'lxml')
-paragraphs = content.find_all('p')
-text = ''
-
-for para in paragraphs:
-    text += para.text
+text=fileread(file1)
 
 
-def SpecialCharacterRemoval(text):
-    result = re.sub(r"['[['(',')'@\'?\.:;$%_]", "", text)
-    result = result.replace(']', r"")
-    return(result)
- 
-
-def Findgrams(articles,num):
-    d=SpecialCharacterRemoval(articles)
-    token = nltk.word_tokenize(d)
-    Ngrams= ngrams(token,num)
-    X=Counter(Ngrams)
-    print("Maximum Repeated Pairs",X.most_common(10))
-    
+print(Findgrams(text,2)) 
+print(Findgrams(text,3))
 
 
-Findgrams(text,1)  
-Findgrams(text,2)
+
